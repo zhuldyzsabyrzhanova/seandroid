234c924855c0e77a42abc136c260cf9fce296bc0
zhuldyzsabyrzhanova <sabyrzhanova95@gmail.com>
2016-02-05 17:58:11 +0600

    commit 6 ttime

--- app/src/main/java/kz/kazak/eldar/se_lab1/MainActivity.java
+++ app/src/main/java/kz/kazak/eldar/se_lab1/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         setSupportActionBar(toolbar);
-    //Zhuldyz comment
+    //Zhuldyz comment asaj
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
